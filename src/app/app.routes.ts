31d346c369b581cba0aa00090b5f771c6e9599a0
varsha <varshapramodmahajan2004@gmail.com>
2025-09-09 20:34:52 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ResumeComponent } from '../components/pages/resume/resume.component';
+import { ProjectsComponent } from '../components/pages/projects/projects.component';
+import { ContactComponent } from '../components/pages/contact/contact.component';
+import { HomeComponent } from '../components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' }, // 👈 your personal home screen
+  { path: 'resume', component: ResumeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' } // fallback
+];
